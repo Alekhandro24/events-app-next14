bc630471eaa7d2087c9df6587d03f616c7554e6e
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-12-20 16:47:42 +0200

    Create MobileNav.tsx

---
+++ components/shared/MobileNav.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
+import NavItems from "./NavItems";
+
+const MobileNav = () => {
+  return (
+    <nav className="md:hidden">
+      <Sheet>
+        <SheetTrigger className="align-middle">
+          <Image
+            src="/assets/icons/menu.svg"
+            alt="menu"
+            height={24}
+            width={24}
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+
+          <Separator className="border border-gray-50" />
+
+          <NavItems />
+        </SheetContent>
+      </Sheet>
+    </nav>
+  );
+};
+
+export default MobileNav;

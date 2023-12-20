6205160bcd5820794648c63e702448fbc628e8c7
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-12-20 16:47:22 +0200

    create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-popins",
+});
+
+export const metadata: Metadata = {
+  title: "Smart Events",
+  description: "Smart Events is a platform for events management",
+  icons: {
+    icon: "/assets/images/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}

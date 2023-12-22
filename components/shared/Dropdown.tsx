a91d0e70131b0c271100f6cb3ecb12384cd9c881
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-12-22 21:43:29 +0200

    create shared\Dropdown.tsx

---
+++ components/shared/Dropdown.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { ICategory } from "@/lib/database/models/category.model";
+
+type DropdownProps = {
+  value?: string;
+  onChangeHandler?: () => void;
+};
+
+const Dropdown = ({ onChangeHandler, value }: DropdownProps) => {
+  const [categories, setCategories] = useState<ICategory[]>([]);
+  return (
+    <Select onValueChange={onChangeHandler} defaultValue={value}>
+      <SelectTrigger className="select-field">
+        <SelectValue placeholder="Category" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="light">Light</SelectItem>
+        <SelectItem value="dark">Dark</SelectItem>
+        <SelectItem value="system">System</SelectItem>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default Dropdown;

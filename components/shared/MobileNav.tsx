06af7ee35fe36448b9e0f3757e32d8e074633fb1
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-12-26 16:12:40 +0200

    Update MobileNav.tsx

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -20,7 +20,7 @@ const MobileNav = () => {
         </SheetTrigger>
         <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
           <Image
-            src="/assets/images/logo.svg"
+            src="../../public/assets/images/logo.svg"
             alt="logo"
             width={128}
             height={38}

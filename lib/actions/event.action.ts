83d5dc837e0d67e026c6e2a629d4b75baa3c840f
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-12-27 23:14:32 +0200

    Create event.action.ts

---
+++ lib/actions/event.action.ts
@@ -0,0 +1,63 @@
+"use server";
+
+import { CreateEventParams } from "@/types";
+import { handleError } from "../utils";
+import { connectToDatabase } from "../database";
+import User from "../database/models/user.model";
+import Event from "../database/models/event.model";
+import Category from "../database/models/category.model";
+
+const populateEvent = async (query: any) => {
+  return query
+    .populate({
+      path: "organizer",
+      model: User,
+      select: "_id firstName lastName",
+    })
+    .populate({
+      path: "category",
+      model: Category,
+      select: "_id name",
+    });
+};
+
+export const createEvent = async ({
+  userId,
+  event,
+  path,
+}: CreateEventParams) => {
+  try {
+    await connectToDatabase();
+
+    const organizer = await User.findById(userId);
+    if (!organizer) {
+      throw new Error("Organizer not found");
+    }
+
+    const newEvent = await Event.create({
+      ...event,
+      category: event.categoryId,
+      organizer: userId,
+    });
+
+    return JSON.parse(JSON.stringify(newEvent));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const getEventById = async (eventId: string) => {
+  try {
+    await connectToDatabase();
+
+    const event = await populateEvent(Event.findById(eventId));
+
+    if (!event) {
+      throw new Error(`No event with id ${eventId}`);
+    }
+
+    return JSON.parse(JSON.stringify(event));
+  } catch (error) {
+    handleError(error);
+  }
+};

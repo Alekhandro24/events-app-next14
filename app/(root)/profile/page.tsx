4df1a02881900288ffd9a34748d018aa34667597
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-12-29 18:55:24 +0200

    Update page.tsx

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,7 +1,10 @@
 import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
 import { getEventsByUser } from "@/lib/actions/event.action";
+import { getOrdersByUser } from "@/lib/actions/order.action";
+import { IOrder } from "@/lib/database/models/order.model";
 import { auth } from "@clerk/nextjs";
+import events from "events";
 import Link from "next/link";
 import React from "react";
 
@@ -9,6 +12,11 @@ const ProfilePage = async () => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
 
+  const orders = await getOrdersByUser({ userId, page: 1 });
+
+  const orderEvents = orders?.data.map((order: IOrder) => order.event) || [];
+
+  console.log({ orderEvents });
   const organizedEvents = await getEventsByUser({ userId, page: 1 });
   return (
     <>
@@ -23,9 +31,9 @@ const ProfilePage = async () => {
         </div>
       </section>
 
-      {/* <section className="wrapper my-8">
+      <section className="wrapper my-8">
         <Collection
-          data={events?.data}
+          data={orderEvents?.data}
           emptyTitle="No event ticket purchased yet"
           emptyStateSubtext="No worries - plenty of exciting events to explore!"
           collectionType="My_Tickets"
@@ -34,10 +42,7 @@ const ProfilePage = async () => {
           totalPage={2}
           urlParamName="ordersPage"
         />
-      </section> */}
-
-      {/* { My Events} */}
-      {/* {Events Organized} */}
+      </section>
 
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">

c6c5237125437cbc374b83585f8c4828f3aabdd4
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-12-29 18:55:21 +0200

    Update page.tsx

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -41,7 +41,7 @@ const EventDetails = async ({
               <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
                 <div className="flex gap-3">
                   <p className="p-bold-20 rounded-full px-5 py-2 bg-green-500/10 text-green-700">
-                    {event.isFree ? "FREE" : `${event.price}`}
+                    {event.isFree ? "FREE" : `$${event.price}`}
                   </p>
 
                   <p className="p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5 text-gray-500">

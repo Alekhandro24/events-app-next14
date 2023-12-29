ac7ea049f8df81d6b0c1387474165e203380a691
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-12-29 17:15:03 +0200

    update events\[id]\update\page.tsx

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,10 +1,19 @@
 import EventForm from "@/components/shared/EventForm";
+import { getEventById } from "@/lib/actions/event.action";
 import { auth } from "@clerk/nextjs";
 import React from "react";
 
-const UpdateEvent = () => {
+type UpdateEventProps = {
+  params: {
+    id: string;
+  };
+};
+
+const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
+
+  const event = await getEventById(id);
 
   return (
     <>
@@ -15,7 +24,12 @@ const UpdateEvent = () => {
       </section>
 
       <div className="wrapper my-8">
-        <EventForm userId={userId} type="Update" />
+        <EventForm
+          userId={userId}
+          type="Update"
+          eventId={event._id}
+          event={event}
+        />
       </div>
     </>
   );

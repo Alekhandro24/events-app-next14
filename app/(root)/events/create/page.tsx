7dc924083e69a3ec7e2b05cbfe5f17dc7f6ad2df
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-12-30 19:58:10 +0200

    update events\create\page.tsx

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -10,7 +10,7 @@ const CreateEvent = () => {
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <h3 className="wrapper h2-bold text-center sm:text-left">
-          CreateEvedfsdfnt
+          Create Event
         </h3>
       </section>
 

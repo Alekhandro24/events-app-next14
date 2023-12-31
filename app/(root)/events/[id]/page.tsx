81a8dfac7789203857dee5ac59d51f81d6abc619
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-12-31 10:32:26 +0200

    update events\[id]\page.tsx

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -111,9 +111,9 @@ const EventDetails = async ({
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
           collectionType="All_Events"
-          limit={6}
-          page={1}
-          totalPage={2}
+          limit={3}
+          page={searchParams.page as string}
+          totalPage={relatedEvents?.totalPages}
         />
       </section>
     </>
